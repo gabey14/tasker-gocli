aa12144498eeb4734fa740ae1aeb689a751211fa
gabey14 <abeygeorge14@gmail.com>
2022-03-04 18:23:43 +0530

    completed init functionality

--- cmd/init.go
+++ cmd/init.go
@@ -1,9 +1,27 @@
 package cmd
 
+import (
+	"github.com/gabey14/tasker-gocli/taskercli"
+	"github.com/spf13/cobra"
+)
 
-func init(){
+func init() {
 
-	var(
-		
+	var (
+		initCmdDesc     = "Initialize a new todo list in the current directory"
+		initCmdLongDesc = `Initializes a new todo list in the current directory.
+		For more information, see https://tasker.io/docs/`
 	)
+
+	var initCmd = &cobra.Command{
+		Use:     "init",
+		Aliases: []string{"i"},
+		Short:   initCmdDesc,
+		Long:    initCmdLongDesc,
+		Run: func(cmd *cobra.Command, args []string) {
+			taskercli.NewApp().InitializeRepo()
+		},
+	}
+
+	rootCmd.AddCommand(initCmd)
 }
