fcc6d2c0a87d5a4f79e457205db822600fc423e6
gabey14 <abeygeorge14@gmail.com>
2022-03-05 08:51:09 +0530

    completed delete functionality

---
+++ cmd/delete.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"strings"
+
+	"github.com/gabey14/tasker-gocli/taskercli"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+
+	var (
+		deleteCmdDesc    = "Deletes todos"
+		deleteCmdExample = `To delete todo with id 35
+		tasker d 35
+		tasker delete 35
+		
+		Note, this will also free up the id 35`
+
+		deleteCmdLongDesc = `Deletes todos with the given id. For more information, see https://tasker.io/docs/`
+	)
+
+	var deleteCmd = &cobra.Command{
+		Use:     "delete [id]",
+		Aliases: []string{"d", "rm"},
+		Short:   deleteCmdDesc,
+		Long:    deleteCmdLongDesc,
+		Example: deleteCmdExample,
+		Run: func(cmd *cobra.Command, args []string) {
+			taskercli.NewApp().DeleteTodo(strings.Join(args, " "))
+		},
+	}
+
+	rootCmd.AddCommand(deleteCmd)
+}
